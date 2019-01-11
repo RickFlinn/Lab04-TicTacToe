8b227bfbcef3ca6de1a6c9845da73b158d04a7c8
RickFlinn <mriodine@gmail.com>
2019-01-11 11:17:16 -0800

    Added game board display, and added some game logic.

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab04_TicTacToe.Classes;
 
 namespace Lab04_TicTacToe
 {
@@ -6,7 +7,10 @@ namespace Lab04_TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("In main, show example board");
+            Game nugame = new Game(new Player(), new Player());
+            Player winner = nugame.Play();
+            Console.WriteLine(winner.Name);
         }
     }
 }
